5bbd6aa593aaed11d9af507bc142cab49021cab4
Hack Reactor Student <student@hackreactor.com>
2017-03-29 17:51:34 -0700

    Pair functional Pattern

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // create new object
+  // object has 1 property of counter = 0 
+  var stackMethodObject = {};
+  stackMethodObject.counter = 0;
+  _.extend(stackMethodObject, stackMethodObject.methods)
+  return stackMethodObject
 };
 
-var stackMethods = {};
+stackMethodObject.methods = {};
+
+// create object with following methords
+	// use _.extend(destination, *sources)  to add functions to main object
+stackMethodObject.methods.push = function(value) { 
+
+
+}
+// create push
+	// add element to object at the end 
+	// increase counter this.counter ++
+stackMethodObject.methods.pop = function() { 
+// create pop 
+	// remove element from object from the end 
+	// decrease counter this.counter --
+} 
+
+stackMethodObject.methods.size = function() { 
+// create size 
+	// return this.counter 
+
+} 
+
 
 
