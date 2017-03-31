8b3552e25a2ac08f2ea4e83ff1023c34f947a0f1
Hack Reactor Student <student@hackreactor.com>
2017-03-31 16:43:02 -0700

    SET COMPLETE

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.size = 0; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+	this[item] = item
 };
 
 setPrototype.contains = function(item) {
+	var result = false
+	for (var key in this){
+		if(key === item){
+			result = true
+		}
+	}
+	return result
 };
 
 setPrototype.remove = function(item) {
+	delete this[item]
 };
 
 /*
