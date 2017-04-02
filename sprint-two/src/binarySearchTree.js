687cbee88ea7f039c92ed5e94937fe53c4c4bda4
Hack Reactor Student <student@hackreactor.com>
2017-04-01 18:09:12 -0700

    Reworking Hash

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,40 @@
 var BinarySearchTree = function(value) {
+
+	var storage = {}; 
+	storage.tree = {};
+
+
+storage.left = function() {
+
+}
+
+storage.right = function() {
+
+}
+
+storage.insert = function(value) {
+
+	if(value)
+		storage.tree[value] =value;
+	
+}
+
+storage.contains = function(value) {
+	return (storage.tree.indexOf(value) !== -1);
+}
+
+storage.depthFirstLog = function(callback) {
+
+	return callback(storage.tree);
+}
+
+console.log(storage.tree);
+return storage;
+
 };
+
+
+
 
 
 /*
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -9,46 +9,42 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  	var bucket = [k,v]
+  	this._storage.set(index,bucket)
   
-  	var add = function(value, key, array) {
+  if (this._storage.get(index)) {
 
-  		if( value = index) {
-  			array[key].push(this.storage.set(index,v));
-  		}
-  	}
-  if(this._storage.get(index)) {
+  	
+  })
 
-
-  	this._storage.each(add);
-	}
-
-	else {
-		this._storage.set(index,v);
-	}
 }
+
 
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var result = this._storage.get(index);
+  var value 
+console.log(this._storage.get(index));
+  if(result === undefined){
+  	return undefined
+  } else {
+  for( var i =0; i<result.length; i++){
+  	if (result[i] === k){
+  		value = i
+  	}
 
-	//console.log(this);
-  return this._storage.get(index);
-
-
+  	return result[1];
+  }
+  
+}
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  var call = function(value,key,array) {
-
-  	if(value = index) {
-
-  		array.splice(key,1);
-  	}
-  }
-
-  this._storage.each(call);
+ this._storage.set(index,undefined)
 
 
 
